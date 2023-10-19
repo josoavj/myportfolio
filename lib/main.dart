f0928a43cd86582e713b0a73db243c2735fc1729
Josoa <josoavonjiniaina13@gmail.com>
2023-10-19 12:42:25 +0400

    Ajout du fichier login pour prof et etudiant

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:lvlmind/animate.dart';
 //import 'package:flutter/widgets.dart';
 
 // ignore: constant_identifier_names
 const d_color = Color.fromARGB(255, 11, 73, 173);
 void main() {
   runApp(lvlmind());
-  home:
-  Scaffold(
-    appBar: AppBar(
-      title: const Text("lvlmind"),
-    ),
-  );
 }
 
 // ignore: camel_case_types
