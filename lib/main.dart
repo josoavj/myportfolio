f5c2a0bb33e6752cee3591b473e0b9e1f1c7324a
Josoa <josoavonjiniaina13@gmail.com>
2023-10-19 12:27:45 +0400

    Reverse mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,23 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 //import 'package:flutter/widgets.dart';
 
+// ignore: constant_identifier_names
 const d_color = Color.fromARGB(255, 11, 73, 173);
 void main() {
   runApp(lvlmind());
+  home:
+  Scaffold(
+    appBar: AppBar(
+      title: const Text("lvlmind"),
+    ),
+  );
 }
 
 // ignore: camel_case_types
-class lvlmind extends StatelessWidget {}
+class lvlmind extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
--- test/login.dart
+++ test/login.dart
@@ -1,6 +1,9 @@
+// ignore_for_file: prefer_const_constructors, unused_import, prefer_const_literals_to_create_immutables, use_key_in_widget_constructors
+
 import 'package:flutter/material.dart';
 import 'package:lvlmind/animate.dart';
 import 'package:lvlmind/main.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginPage extends StatelessWidget {
   @override
@@ -30,13 +33,42 @@ class LoginPage extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 DelayedAnimation(
-                    delay: 1500,
-                    Child: Text(
-                      "Connect email address",
-                    ))
+                  delay: 1500,
+                  Child: Text(
+                    "Connect ID",
+                    style: GoogleFonts.poppins(
+                      color: d_color,
+                      fontSize: 25,
+                      FontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 22),
+                DelayedAnimation(
+                  delay: 2500,
+                  Child: Text(
+                    "It's recommended to connect your ID for us",
+                    style: GoogleFonts.poppins(
+                      color: Colors.grey[600],
+                      fontSize: 16,
+                      FontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
               ],
             ),
-          )
+          ),
+          SizedBox(height: 35),
+          // LoginForm(),
+          SizedBox(height: 125),
+          /*
+          DelayedAnimation(
+              delay: 5500,
+               Child: ElevatedButton.styleFrom(
+                shape: StadiumBorder(),
+                primary: d_color,
+              ))
+           */
         ]),
       ),
     );
