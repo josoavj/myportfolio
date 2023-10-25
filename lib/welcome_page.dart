56f52c28138c71aef35064034c1abb658c298546
Josoa <josoavonjiniaina13@gmail.com>
2023-10-25 11:26:11 +0400

    Modification de la page d'acceuil

---
+++ lib/welcome_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:portfolio/Skills.dart';
+import 'package:portfolio/main.dart';
+import 'package:portfolio/Profile.dart';
+
+class welcomepage extends StatefulWidget {
+  const welcomepage({super.key});
+
+  @override
+  // ignore: no_logic_in_create_state
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+}
