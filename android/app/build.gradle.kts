758f6d677a6b6bc20bd8a8e4593a4695a7742daa
Josoa Vonjiniaina <josoavonjiniaina13@gmail.com>
2025-12-13 17:46:18 +0300

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.shadowcraft.myportfolio"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "29.0.14206865"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
